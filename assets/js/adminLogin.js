5330a8929d394c3dbf3c666e8e7a3566987cf9a4
AnnapooraniBabu <poojasri92749@gmail.com>
2022-01-13 23:00:15 +0530

    fixed code spells

--- assets/js/adminLogin.js
+++ assets/js/adminLogin.js
@@ -2,9 +2,9 @@ function submitHandler(event){
     event.preventDefault();
 
     const adminEmailId = "Admin@gmail.com";
-    // console.log(adminEmailId);
+    
     const adminPassword = "Admin@Fidaa";
-    // console.log(adminPassword);
+    
 
     const adminMailInput = document.getElementById("emailId").value;
     console.log(adminMailInput);
@@ -14,7 +14,7 @@ function submitHandler(event){
         const alert = document.getElementById("alert");
         alert.innerHTML = "Successfully LoggedIn";
         window.location.href="./../pages/musicpage.html";
-        return;
+        
     }
     else{
         const errorAlert = document.getElementById("alert");
