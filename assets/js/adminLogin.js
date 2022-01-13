404c74e9383793be547479952db3c54aadf5db56
AnnapooraniBabu <poojasri92749@gmail.com>
2022-01-13 22:55:58 +0530

    worked on adminlogin feature

--- assets/js/adminLogin.js
+++ assets/js/adminLogin.js
@@ -2,19 +2,22 @@ function submitHandler(event){
     event.preventDefault();
 
     const adminEmailId = "Admin@gmail.com";
+    // console.log(adminEmailId);
     const adminPassword = "Admin@Fidaa";
+    // console.log(adminPassword);
 
-    const adminMailInput = document.getElementById("emailId");
-    const adminPass = document.getElementById("password");
-
-    if(adminEmailId==adminMailInput && adminPassword==adminPass){
+    const adminMailInput = document.getElementById("emailId").value;
+    console.log(adminMailInput);
+    const adminPass = document.getElementById("password").value;
+    console.log(adminPass);
+    if(adminEmailId===adminMailInput && adminPassword === adminPass){
         const alert = document.getElementById("alert");
         alert.innerHTML = "Successfully LoggedIn";
         window.location.href="./../pages/musicpage.html";
+        return;
     }
     else{
         const errorAlert = document.getElementById("alert");
-        errorAlert.innerHTML = "Invalid Login Credentials";
-        
+        errorAlert.innerHTML = "Invalid Login Credentials"; 
     }
     }
